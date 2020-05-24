e349e4e8126682f38feae6e066697361575947bf
TaeHyoungKwon <kth5604@gmail.com>
2020-05-24 22:50:59 +0900

    add code for calculating recommend clothes

--- write_every_time_script.py
+++ write_every_time_script.py
@@ -3,14 +3,24 @@ from datetime import datetime, timedelta
 from os import environ
 from selenium import webdriver
 
-from constants import SECRET_BOARD_URL, weather_text, WEATHER_AUTO_TEXT, WEEK_DAY, DATE_FORMAT, DATETIME_FORMAT
+from constants import (
+    SECRET_BOARD_URL,
+    weather_text,
+    WEATHER_AUTO_TEXT,
+    WEEK_DAY,
+    DATE_FORMAT,
+    DATETIME_FORMAT,
+    OVER_30_DEGREE,
+    CLOTHES_TEXT,
+)
 from crawl_weather_info import crawl_naver_weather_info
+from helpers import choose_clothes
 
 
 class WeatherBot:
     def __init__(self):
         options = webdriver.ChromeOptions()
-        options.add_argument('headless')
+        options.add_argument("headless")
         self.driver = webdriver.Chrome(chrome_options=options)
 
     @staticmethod
@@ -25,10 +35,26 @@ class WeatherBot:
         detail_info = ""
         if not (uv or rainfall):
             detail_info = "자외선, 강수 정보가 제공되지 않고 있습니다."
-        if not rainfall:
-            detail_info = uv.text
-        if not uv:
-            detail_info = rainfall.text
+        else:
+            if not rainfall:
+                detail_info = uv.text
+            if not uv:
+                detail_info = rainfall.text
+
+        min_temperature = weather_info_html.find("span", {"class": "min"}).text
+        max_temperature = weather_info_html.find("span", {"class": "max"}).text
+
+        int_min_temperature = int(min_temperature.replace("˚", ""))
+        int_max_temperature = int(max_temperature.replace("˚", ""))
+
+        if int_max_temperature > 30:
+            clothes_message = OVER_30_DEGREE
+        if int_min_temperature < 0:
+            clothes_message = OVER_30_DEGREE
+        else:
+            clothes_message = CLOTHES_TEXT.format(
+                clothes_first=choose_clothes(int_min_temperature), clothes_seconds=choose_clothes(int_max_temperature)
+            )
 
         weather_info = {
             "today_date": local_now.strftime(DATE_FORMAT),
@@ -36,10 +62,11 @@ class WeatherBot:
             "today_datetime": local_now.strftime(DATETIME_FORMAT),
             "today_temp": weather_info_html.find("span", {"class": "todaytemp"}).text + " 도",
             "cast_txt": weather_info_html.find("p", {"class": "cast_txt"}).text,
-            "min_temperature": weather_info_html.find("span", {"class": "min"}).text,
-            "max_temperature": weather_info_html.find("span", {"class": "max"}).text,
+            "min_temperature": min_temperature,
+            "max_temperature": max_temperature,
             "sensible": weather_info_html.find("span", {"class": "sensible"}).text,
             "detail_info": detail_info,
+            "clothes_message": clothes_message,
         }
         return WEATHER_AUTO_TEXT.format(**weather_info)
 
@@ -76,7 +103,7 @@ class WeatherBot:
     def submit_auto_weather_info(self):
         self._click_form()
         print(self.reform_weather_info())
-        #self._submit_weather_info(contents=self.reform_weather_info())
+        # self._submit_weather_info(contents=self.reform_weather_info())
         print("완료스!")
 
 
